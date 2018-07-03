55bbfbcdf075e81eae680b443c472fa0fa2999c9
Szymon Oleszek <szymqwerty@gmail.com>
2018-07-03 17:34:57 +0200

    working app v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,41 @@
-let dropDownMenuOptions = document.querySelectorAll('.dropdown-item')
-let typeOfUnit = "pounds"
+let typeOfUnit = "pounds" 
 let inputValue = ''
 let kgOutput = ''
 let ozOutput = ''
 let gOutput = ''
 let lbsOutput = ''
 
-
-dropDownMenuOptions.forEach(el => {
-    el.addEventListener('click', () => {
-        document.querySelector('.input-search').placeholder = el.firstChild.data
-        changeDisplaingCards(document.querySelector('.input-search').placeholder)
-        // if(placeholder === z którymś z opcji ){
-        //     to którąś z opcji zamień na typeOfUnit
-        // }
-
-        changeTypeOfUnit(el)
+const addEventListenersToDropDownMenu = () => {
+    document.querySelectorAll('.dropdown-item').forEach(el => {
+        el.addEventListener('click', () => {
+            document.querySelector('.input-search').placeholder = el.firstChild.data
+            changeDisplaingCards(document.querySelector('.input-search').placeholder)
+            changeTypeOfUnit(el)
+            convertValues()
+            displayResult()
+        })
+    })
+}
+const addEventListenerToTextInput = () => {
+    document.querySelector('#lbs-input').addEventListener('input', (e) => {
+        inputValue = e.target.value
         convertValues()
         displayResult()
     })
-})
-
-
-document.querySelector('#lbs-input').addEventListener('input', (e) => {
-    let lbs = e.target.value
-    inputValue = e.target.value
-    document.querySelector('#grams-output').innerHTML = `<span>${lbs / 0.0022046}</span>`
-    document.querySelector('#kg-output').innerHTML = `<span>${lbs / 2.2046}</span>`
-    document.querySelector('#oz-output').innerHTML = `<span>${lbs * 16}</span>`
-    setZero(e)
-})
-
-const setZero = (e) => {
-    if (e.target.value === null || e.target.value === 0 || e.target.value === "") {
-        document.querySelector('#grams-output').innerHTML = ``
-        document.querySelector('#kg-output').innerHTML = ``
-        document.querySelector('#oz-output').innerHTML = ``
-    }
 }
-
-
-const dropDownPlaceholder = () => {
-
-    document.querySelector('.input-search').placeholder = i
-}
-
 const changeTypeOfUnit = (el) => {
     typeOfUnit = el.firstChild.data.toLowerCase()
 }
 const changeDisplaingCards = (changedOption) => {
-
     document
-    .querySelectorAll('.card-title')
-    .forEach((el) => {
-        // console.log("el.firstChild: ", el.firstChild.data)
-        // console.log("typeofunit: ", typeOfUnit)
-        if (el.firstChild.data === changedOption) {
-            el.firstChild.data = typeOfUnit.charAt(0).toUpperCase() + typeOfUnit.slice(1)
-        }
-
-    })
-  
+        .querySelectorAll('.card-title')
+        .forEach((el) => {
+            if (el.firstChild.data === changedOption) {
+                el.firstChild.data = typeOfUnit.charAt(0).toUpperCase() + typeOfUnit.slice(1)
+                el.nextElementSibling.id = el.firstChild.data + '-output'   
+            }
+        })
 }
-
 const convertValues = () => {
     switch (typeOfUnit) {
         case "pounds":
@@ -89,12 +62,12 @@ const convertValues = () => {
             console.log('default')
     }
 }
-
 const displayResult = () => {
-
+   if(document.querySelector('#Ounces-output'))document.querySelector('#Ounces-output').innerHTML = ozOutput
+   if(document.querySelector('#Kilograms-output'))document.querySelector('#Kilograms-output').innerHTML = kgOutput
+   if(document.querySelector('#Grams-output'))document.querySelector('#Grams-output').innerHTML = gOutput
+   if(document.querySelector('#Pounds-output'))document.querySelector('#Pounds-output').innerHTML = lbsOutput
 }
 
-const switchCard = () => {
-
-   
-}
+addEventListenerToTextInput()
+addEventListenersToDropDownMenu()
